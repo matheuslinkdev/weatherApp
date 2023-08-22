d0aa3a5f00548d74d0d62467df15f4f71303a98b
Matheus Link <122500941+MthsLnk-gthb@users.noreply.github.com>
2023-08-22 19:16:31 -0300

    Add files via upload
    I've added the files of the project

---
+++ main.js
@@ -0,0 +1,40 @@
+const apiKey = "4db257998d46b9769c5706ab42f4b4e5"
+const searchBtn = document.getElementById("buscarCidade")
+
+async function buscarCidade(city){
+    try {
+      const dados = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`
+      ).then((response) => response.json());
+
+      if (dados.cod === "404") {
+        throw new Error("Cidade não encontrada");
+      }
+      renderData(dados);
+    } catch (error) {
+      handleError(error.message);
+    }
+}
+
+function buttonClick(){
+    const cityInput = document.querySelector("#inputCidade").value;
+    buscarCidade(cityInput)
+}
+
+function handleError(errorMessage) {
+  document.querySelector("#nomeCidade").innerHTML = "Erro: " + errorMessage;
+  document.querySelector(".temperatura").innerHTML = "";
+  document.querySelector(".textoCondicao").innerHTML = "";
+  document.querySelector(".humidade-do-ar").innerHTML = "";
+  document.querySelector(".imagem-da-condicao").src = "";
+  document.querySelector(".velocidade-do-vento").innerHTML = "";
+}
+
+function renderData(dados){
+ document.querySelector("#nomeCidade").innerHTML = "Tempo em " + dados.name + ", " + dados.sys.country;
+ document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C";
+ document.querySelector(".textoCondicao").innerHTML = dados.weather[0].description;
+ document.querySelector(".humidade-do-ar").innerHTML = "Humidade do ar: " + dados.main.humidity + "%";
+ document.querySelector(".imagem-da-condicao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+ document.querySelector(".velocidade-do-vento").innerHTML = "Vento: " + Math.floor(dados.wind.speed) + "Km/h"
+}
